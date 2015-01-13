f72834a6a19a285ea6664a35e95c4cbfb87bd3da
Nick Maglio <foundchain418@hotmail.com>
2015-01-13 18:12:03 -0500

    Added another comment (untested)
    Signed-off by: Colin

--- Getting Started/src/Robot.cpp
+++ Getting Started/src/Robot.cpp
@@ -17,6 +17,7 @@
 // Change #16 Mr. Rudolph after repository restore - test #2
 // Change #18 Mr. Rudolph after repository restore - test #3
 // Change #19 C. Noll rocks....
+// Change #20 Nick Maglio is better....
 class Robot: public IterativeRobot
 {
 
