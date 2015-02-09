d5413ad6f90073057c387b2bc850810288463f1b
nollchr <nollchr@yahoo.com>
2015-02-08 21:26:40 -0500

    Added low pass filter class.

---
+++ RecycleRush/src/LowPassFilter.h
@@ -0,0 +1,24 @@
+/*
+ * LowPassFilter.h
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: nollchr
+ */
+
+#ifndef SRC_LOWPASSFILTER_H_
+#define SRC_LOWPASSFILTER_H_
+
+class LowPassFilter {
+private:
+	double fCurrentOutput = 0.0;
+	double fAlpha = 0.0;
+	double CalculateAlpha(double fCutoffHz, double fSampleRateHz);
+public:
+	LowPassFilter();
+	LowPassFilter(double fCutoffHz, double fSampleRateHz);
+	double Update(double fNewVal);
+	void Reset(void);
+	virtual ~LowPassFilter();
+};
+
+#endif /* SRC_LOWPASSFILTER_H_ */
