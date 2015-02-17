fcb12177847e7355397252f1cf03782a477f3d38
nollchr <nollchr@yahoo.com>
2015-02-17 18:32:46 -0500

    Preliminary drive straight with triggers...

--- RecycleRush/src/MotionData.cpp
+++ RecycleRush/src/MotionData.cpp
@@ -9,7 +9,6 @@
 
 MotionData::MotionData() {
 	// TODO Auto-generated constructor stub
-
 }
 
 MotionData::MotionData(Accelerometer* accel) {
@@ -21,14 +20,25 @@ MotionData::~MotionData() {
 }
 
 void MotionData::UpdateData() {
-	mAcceleration += mAccel->GetY() * GsToMeters;;
-	mVelocity += mAcceleration * mDeltaTime;
-	mDistance += mVelocity * mDeltaTime;
-	mOdometer += mVelocity * mDeltaTime;
+	double accel = DEADBAND(mAccel->GetX(), 0.05);
+	double dT = UpdateTimer.Get();
+
+	mAcceleration += accel * GsToMeters;;
+	mVelocity += mAcceleration * dT;
+	mDistance += mVelocity * dT;
+	mOdometer += mVelocity * dT;
+	UpdateTimer.Reset();
 }
 
+void MotionData::Start() {
+	UpdateTimer.Start();
+}
 void MotionData::ResetDistance() {
 	mDistance = 0.0;
+}
+
+void MotionData::ResetOdometer() {
+	mOdometer = 0.0;
 }
 
 double MotionData::GetDistance() {
@@ -51,4 +61,6 @@ void MotionData::Stop() {
 	mAcceleration = 0.0;
 	mVelocity = 0.0;
 	mDistance = 0.0;
+	UpdateTimer.Stop();
+	UpdateTimer.Reset();
 }
